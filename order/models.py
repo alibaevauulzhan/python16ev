ef58c190c1775cf64c2113a56aa9cc9285fd404e
alibaevauulzhan <97526174+alibaevauulzhan@users.noreply.github.com>
2022-01-13 21:12:07 +0600

    order

--- order/models.py
+++ order/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+from model_utils import Choices
+from model_utils.fields import StatusField
+
+from product.models import CreatedAtModel
+User = get_user_model()
+
+class Order(CreatedAtModel):
+    STATUS = Choices('In progress', 'Canceled', 'Finished')
+    total_sum = models.DecimalField(mas_digits=10, decimal_places=2, default=0)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='orders')
+    products = models.ManyToManyField('product.Product')
+    order_status = StatusField()
+
+    class Meta:
+        ordering = ['-created_at']
+        db_table = 'order'
+
+    def __str(self):
+        return f'Заказ № {self.id} от {self.created_at.strftime("%d-%m-%Y-%H-%M")}'
